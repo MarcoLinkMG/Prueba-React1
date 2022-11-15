0befbcb64794f25864bb60e1b02e71648acc3394
Marco Gallardo <marco@linkmg.cl>
2022-11-14 23:24:44 -0300

    Cuarto Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MiApi from "./components/MiApi"
 import portada from './dragonballsuper.jpg'
+import Piepagina from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <div className='header'>
         <img src={portada} className='portada'/>
       </div>
       <div className='section'>
         <MiApi />
       </div>
-    </>
+      <footer>
+        <Piepagina />
+      </footer>
+    </div>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Piepagina = () => {
+
+    return (
+        <div>
+            <h2>Sitio web para fanaticos de Dragon Ball Super - Desarrollado por Marco Gallardo</h2>
+        </div>
+    )
+
+}
+
+export default Piepagina;
